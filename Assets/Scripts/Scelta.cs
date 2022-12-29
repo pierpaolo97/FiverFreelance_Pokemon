07e60962bad04fdc79f5bd55d962ab0a78cd5728
lucadipalma <91740343+lucadipalma@users.noreply.github.com>
2022-12-29 20:45:24 +0100

    Aggiornamento_4

--- Assets/Scripts/Scelta.cs
+++ Assets/Scripts/Scelta.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class Scelta : MonoBehaviour
@@ -21,13 +22,16 @@ public class Scelta : MonoBehaviour
     public GameObject avanti;
     public GameObject startGame;
 
-    GameObject enemy1;
-    GameObject enemy2;
-    GameObject player;
-    GameObject friend;
+    public GameObject enemy1;
+    public GameObject enemy2;
+    public GameObject player;
+    public GameObject friend;
 
     public AudioSource cameraAudio;
-
+    public List<GameObject> gameobjectDaDisattivare_uno;
+    public List<GameObject> gameobjectDaDisattivare_due;
+    public List<GameObject> gameobjectDaDisattivare_tre;
+    public List<GameObject> gameobjectDaDisattivare_quattro;
 
     public void Avanza()
     {
@@ -48,17 +52,38 @@ public class Scelta : MonoBehaviour
             player.GetComponent<Unit>().mosse[i] = mossa_da_istanziare;
         }
 
-        StartCoroutine(example());
+        StartCoroutine(example(scegliPlayer, gameobjectDaDisattivare_uno, scegliCompagno, scegliPlayer, 0));
     }
 
-    IEnumerator example()
+    IEnumerator example(GameObject scegli, List<GameObject> lista, GameObject daAttivare, GameObject daDisattivare, int ultimo)
     {
         int x = UnityEngine.Random.RandomRange(0, 2);
-        cameraAudio.PlayOneShot(scegliPlayer.GetComponent<ScegliPersonaggi>().personaggiDisponibili[scegliPlayer.GetComponent<ScegliPersonaggi>().indexPlayer].GetComponent<Unit>().audioSelezioni[x]);
+        cameraAudio.PlayOneShot(scegli.GetComponent<ScegliPersonaggi>().personaggiDisponibili[scegli.GetComponent<ScegliPersonaggi>().indexPlayer].GetComponent<Unit>().audioSelezioni[x]);
+        foreach(GameObject g in lista)
+        {
+            g.GetComponent<Button>().interactable = false;
+        }
         yield return new WaitWhile(() => cameraAudio.isPlaying);
-        scegliCompagno.SetActive(true);
-        scegliPlayer.SetActive(false);
-        //do something
+
+        daDisattivare.SetActive(false);
+        daAttivare.SetActive(true);
+
+        foreach (GameObject g in lista)
+        {
+            g.GetComponent<Button>().interactable = true;
+        }
+        if (ultimo == 1)
+        {
+            disattivaUltimo();
+        }
+    }
+
+    public void Back1()
+    {
+        scegliPlayer.SetActive(true);
+        scegliCompagno.SetActive(false);
+        //Destroy(friend);
+        Destroy(player);
     }
 
     public void Avanza_2()
@@ -78,20 +103,23 @@ public class Scelta : MonoBehaviour
             friend.GetComponent<Unit>().mosse[i] = mossa_da_istanziare;
         }
         back.SetActive(false);
+        StartCoroutine(example(scegliCompagno, gameobjectDaDisattivare_due, scegliEnemy1, scegliCompagno, 0));
     }
 
-    public void StartGame()
+    public void Back2()
     {
-        ememy1e2.SetActive(false);
+        scegliEnemy1.SetActive(false);
+        scegliCompagno.SetActive(true);
+        //Destroy(friend);
+        Destroy(friend);
+    }
+
+    public void Avanza_3()
+    {
         enemy1 = Instantiate(scegliEnemy1.GetComponent<ScegliPersonaggi>().personaggiDisponibili[scegliEnemy1.GetComponent<ScegliPersonaggi>().indexPlayer]);
         enemy1.name = enemy1.GetComponent<Unit>().unitName;
         enemy1.GetComponent<Unit>().unitID = 2;
-        enemy2 = Instantiate(scegliEnemy2.GetComponent<ScegliPersonaggi>().personaggiDisponibili[scegliEnemy2.GetComponent<ScegliPersonaggi>().indexPlayer]);
-        enemy2.name = enemy2.GetComponent<Unit>().unitName;
-        enemy2.GetComponent<Unit>().unitID = 3;
         battleSystem.enemyPrefab = enemy1;
-        battleSystem.enemy2Prefab = enemy2;
-
         GameObject parentMosseEnemy1 = new GameObject();
         parentMosseEnemy1.name = "Mosse_" + enemy1.GetComponent<Unit>().unitName;
 
@@ -101,6 +129,25 @@ public class Scelta : MonoBehaviour
             mossa_da_istanziare.name = enemy1.GetComponent<Unit>().mosse[i].GetComponent<Mossa>().nomeMossa;
             enemy1.GetComponent<Unit>().mosse[i] = mossa_da_istanziare;
         }
+        StartCoroutine(example(scegliEnemy1, gameobjectDaDisattivare_tre, scegliEnemy2, scegliEnemy1, 0));
+    }
+
+    public void Back3()
+    {
+        scegliEnemy1.SetActive(true);
+        scegliEnemy2.SetActive(false);
+        //Destroy(friend);
+        Destroy(enemy1);
+    }
+
+    public void StartGame()
+    {
+        
+        enemy2 = Instantiate(scegliEnemy2.GetComponent<ScegliPersonaggi>().personaggiDisponibili[scegliEnemy2.GetComponent<ScegliPersonaggi>().indexPlayer]);
+        enemy2.name = enemy2.GetComponent<Unit>().unitName;
+        enemy2.GetComponent<Unit>().unitID = 3;
+        battleSystem.enemyPrefab = enemy1;
+        battleSystem.enemy2Prefab = enemy2;
 
         GameObject parentMosseEnemy2 = new GameObject();
         parentMosseEnemy2.name = "Mosse_" + enemy2.GetComponent<Unit>().unitName;
@@ -112,9 +159,15 @@ public class Scelta : MonoBehaviour
             enemy2.GetComponent<Unit>().mosse[i] = mossa_da_istanziare;
         }
 
+        StartCoroutine(example(scegliEnemy2, gameobjectDaDisattivare_quattro, scegliEnemy2, scegliEnemy2, 1));
+       
+    }
 
+    public void disattivaUltimo()
+    {
         interoMenu.SetActive(false);
         battleSystem.enabled = true;
+        ememy1e2.SetActive(false);
     }
 
     public void ScegliDiNuovo()
