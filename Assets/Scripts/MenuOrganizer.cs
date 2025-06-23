26de952827aea0a8da55b9a59227329139cbecc9
pierpaolo97 <91739395+pierpaolo97@users.noreply.github.com>
2025-06-23 19:02:12 +0200

    animazioni e prefab nuovi pg

--- Assets/Scripts/MenuOrganizer.cs
+++ Assets/Scripts/MenuOrganizer.cs
@@ -68,6 +68,18 @@ public class MenuOrganizer : MonoBehaviour
         Debug.Log("is this working?");
     }
 
+    public void OpenBloodyRoar()
+    {
+        Application.OpenURL("https://www.amazon.it/Bloody-Roar-Nicola-Racca/dp/B0DKP47TT3/ref=sr_1_2?__mk_it_IT=ÅMÅŽÕÑ&crid=2LITGIXCUB7XO&dib=eyJ2IjoiMSJ9.IvuIq10VTmK-LQfkSNJhR_J-xQZzxO4JxQPlHwLskgAUBVd2Mb02X85FbfuJ0UoDa5uHEkU2Uy2cu5pjkTdh5A.h-tTuFGe_LTjYfATiYS-t8g3bG00ZqXTAkjPzeJenis&dib_tag=se&keywords=bloody+roar+fumetto&qid=1750110760&sprefix=bloody+roar+fmetto%2Caps%2C67&sr=8-2");
+        Debug.Log("is this working?");
+    }
+
+    public void OpenComicOfLove()
+    {
+        Application.OpenURL("https://www.amazon.it/Comic-love-Nicola-Racca/dp/B0F9HWTMMZ/ref=sr_1_1?__mk_it_IT=ÅMÅŽÕÑ&crid=1F4H68524BZ1V&dib=eyJ2IjoiMSJ9.ZVeos-K9oBXAys91qg2z2hmZrVNbt-tc6diJp6k2QNM6lBUyFUnhwyiVw0xXcmmWUXu8ZjlwZ2is0YJNFCZyFJcxlYtJKwWdMtECIpkf8XibdMQDbG23cxxfGMR7hiSnX-8H_WEl5JBWPSrvGqZQk5V0GY_eTvnRRv1ODMPCUPJ3akVvxpm8GoQzT9Ime0D-aRs8hjq3oSwB9Z2XLfiyqVZ3UAuAB8MNj2fF4uXq_o_THVhdJuVfWOcNc3TFKLQl4zRNrUJWbVCKv2eKriJpNnR5xAcz-EPCL1vIt2Lb9D4.U3GozbXFZTAYANU4u1pc63iDbGc8zBnZCzLQiFxLfV8&dib_tag=se&keywords=Comic+of+Love&qid=1750110909&sprefix=comic+of+love%2Caps%2C141&sr=8-1");
+        Debug.Log("is this working?");
+    }
+
     IEnumerator LoadSceneTrans(string nameScene)
     {
         if (Random.value < 0.5f)
