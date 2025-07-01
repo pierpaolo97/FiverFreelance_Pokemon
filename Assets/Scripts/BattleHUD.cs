d9f6a1c5a68ca9e0a695b363e8fbc0927da0467c
lucadipalma <91740343+lucadipalma@users.noreply.github.com>
2025-07-01 23:49:14 +0200

    aggiunto buio; cambiato anche i colori di background/testo per gli elementi

--- Assets/Scripts/BattleHUD.cs
+++ Assets/Scripts/BattleHUD.cs
@@ -15,27 +15,52 @@ public class BattleHUD : MonoBehaviour
     public RawImage coloreElemento;
 
     public void SetHUD(Unit unit)
-	{
-		nameText.text = unit.unitName;
-		//levelText.text = "Lvl " + unit.unitLevel;
-		hpSlider.maxValue = unit.maxHP;
-		hpSlider.value = unit.currentHP;
-		HPText.text = hpSlider.value.ToString() + "/" + hpSlider.maxValue.ToString();
+    {
+        nameText.text = unit.unitName;
+        //levelText.text = "Lvl " + unit.unitLevel;
+        hpSlider.maxValue = unit.maxHP;
+        hpSlider.value = unit.currentHP;
+        HPText.text = hpSlider.value.ToString() + "/" + hpSlider.maxValue.ToString();
         elementoText.text = unit.elemento;
 
         switch (unit.elemento)
         {
-            case "VENTO": coloreElemento.color = new Color32(116, 146, 226, 255); break;
-            case "NORMALE": coloreElemento.color = new Color32(255, 255, 255, 255); break;
-            case "TERRA": coloreElemento.color = new Color32(154, 104, 54, 255); break;
-            case "SPAZIO": coloreElemento.color = new Color32(57, 66, 180, 255); break;
-            case "FUOCO": coloreElemento.color = new Color32(255, 67, 67, 255); break;
-            case "ACQUA": coloreElemento.color = new Color32(32, 136, 178, 255); break;
-            case "NESSUNO": coloreElemento.color = new Color32(253, 237, 163, 255); break;
+            case "VENTO":
+                coloreElemento.color = new Color32(116, 146, 226, 255);
+                elementoText.color = Color.black;
+                break;
+            case "NORMALE":
+                coloreElemento.color = new Color32(255, 255, 255, 255);
+                elementoText.color = Color.black;
+                break;
+            case "TERRA":
+                coloreElemento.color = new Color32(154, 104, 54, 255);
+                elementoText.color = Color.black;
+                break;
+            case "SPAZIO":
+                coloreElemento.color = new Color32(57, 66, 180, 255);
+                elementoText.color = Color.white;
+                break;
+            case "BUIO":
+                coloreElemento.color = Color.black; // Background to black
+                elementoText.color = Color.white;   // Text to white
+                break;
+            case "FUOCO":
+                coloreElemento.color = new Color32(255, 67, 67, 255);
+                elementoText.color = Color.black;
+                break;
+            case "ACQUA":
+                coloreElemento.color = new Color32(32, 136, 178, 255);
+                elementoText.color = Color.black;
+                break;
+            case "NESSUNO":
+                coloreElemento.color = new Color32(253, 237, 163, 255);
+                elementoText.color = Color.black;
+                break;
         }
     }
 
-	public void SetHP(Unit Colpito)
+    public void SetHP(Unit Colpito)
 	{
 		hpSlider.value = Colpito.currentHP;
 		if (Colpito.currentHP <= 0)
